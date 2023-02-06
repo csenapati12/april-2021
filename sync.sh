61fb175b018d7e18cdf139581858bd982c4e3ffb
Chaitanya Senapati <csenapati12@gmail.com>
2023-02-06 10:56:42 +0530

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,7 +4,7 @@ cp sync.sh april-2021
 cd april-2021
 git config --global user.email "csenapati12@gmail.com"
 git config --global user.name "csenapati12"
-curl -u "csenapati12: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" https://github.com/csenapati12/april-2021.git
+curl -u "csenapati12:aa" https://github.com/csenapati12/april-2021.git
 git add .
 git status
 git commit -m "add sync code"
